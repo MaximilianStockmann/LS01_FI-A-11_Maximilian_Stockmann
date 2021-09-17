6da9ebe2bcb338d252aca155c8de3fb48ea3e377
MaximilianStockmann <maximilian.stockmann@gmx.de>
2021-09-17 10:00:59 +0200

    Folder structure overhaul

---
+++ Konsolenausgabe/src/Konsolenausgabe.java
@@ -0,0 +1,33 @@
+public class Konsolenausgabe {
+    public static void main(String args[]) {
+        //Code für Aufgabe 1
+        System.out.print("\"Das ist ein Beispielsatz.\"\n");
+        System.out.println("Dies ist ein weiterer " +  "Beispielsatz.");
+
+        //Das ist ein Kommentar
+
+        //Code für Aufgabe 2
+        System.out.println("      *      ");
+        System.out.println("     ***     ");
+        System.out.println("    *****    ");
+        System.out.println("   *******   ");
+        System.out.println("  *********  ");
+        System.out.println(" *********** ");
+        System.out.println("*************");
+        System.out.println("     ***     ");
+        System.out.println("     ***     ");
+
+        //Code für Aufgabe 3
+        double zahl1 = 22.4234234;
+        double zahl2 = 111.2222;
+        double zahl3 = 4.0;
+        double zahl4 = 1000000.551;
+        double zahl5 = 97.34;
+
+        System.out.printf("%.2f\n", zahl1);
+        System.out.printf("%.2f\n", zahl2);
+        System.out.printf("%.2f\n", zahl3);
+        System.out.printf("%.2f\n", zahl4);
+        System.out.printf("%.2f\n", zahl5);
+    }
+}
---
+++ TestProjekt/src/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        // Ausgabe in der Konsole
+        System.out.println("Test");
+    }
+}
