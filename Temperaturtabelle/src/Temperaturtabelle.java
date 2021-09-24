f5f2a8d7663f06cb206286610b39e55ae5c87f03
MaximilianStockmann <maximilian.stockmann@gmx.de>
2021-09-24 09:23:36 +0200

    Temperaturtabelle changed to better reflect task

--- Temperaturtabelle/src/Temperaturtabelle.java
+++ Temperaturtabelle/src/Temperaturtabelle.java
@@ -2,10 +2,10 @@ public class Temperaturtabelle {
     public static void main (String[] args) {
         System.out.printf("%-12s|%10s\n", "Fahrenheit", "Celsius");
         System.out.printf("%s\n", "------------------------");
-        System.out.printf("%-12d|%10.2f\n", -20, -28.8889);
-        System.out.printf("%-12d|%10.2f\n", -10, -23.3333);
-        System.out.printf("%-12d|%10.2f\n", 0, -17.7778);
-        System.out.printf("%-12d|%10.2f\n", 20, -6.6667);
-        System.out.printf("%-12d|%10.2f\n", 30, -1.1111);
+        System.out.printf("%+-12d|%10.2f\n", -20, -28.8889);
+        System.out.printf("%+-12d|%10.2f\n", -10, -23.3333);
+        System.out.printf("%+-12d|%10.2f\n", 0, -17.7778);
+        System.out.printf("%+-12d|%10.2f\n", 20, -6.6667);
+        System.out.printf("%+-12d|%10.2f\n", 30, -1.1111);
     }
 }
